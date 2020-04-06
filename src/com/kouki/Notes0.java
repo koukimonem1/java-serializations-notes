a0e085e81aaa21538a174f2ea65b3bf7e3b75d35
Abdelmonem <abdelmonem.kouki@gmail.com>
2020-04-06 17:06:21 +0200

    Initial commit

---
+++ src/com/kouki/Notes0.java
@@ -0,0 +1,8 @@
+package com.kouki;
+
+public class Notes0 {
+ // sérializer un objet consiste à le convertir en un tableau d'octets, que l'on peut ensuite écrire dans un fichier, envoyer sur un réseau au travers d'une socket
+ // Ce mécanisme existe depuis les débuts de l'API Java I/O
+ // Il suffit de passer tout objet qui implémente l'interface Serializable à une instance de ObjectOutputStream pour sérialiser un objet
+
+}
